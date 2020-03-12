75836df2dbb054e8aea8bd602386c5bab44db932
MKS1030 <manish10mks@gmail.com>
2020-03-12 14:35:56 +0530

    n/w

--- src/utils/CheckConnection.js
+++ src/utils/CheckConnection.js
@@ -1,14 +1,19 @@
 import { useState, useEffect } from 'react'
 import NetInfo from "@react-native-community/netinfo";
 
+let currentNetwork;
+
+NetInfo.fetch().then(state => {
+    currentNetwork = state.isConnected;
+});
 
 const CheckConnection = () => {
-    const [netInfo, setNetInfo] = useState(false)
+    const [netInfo, setNetInfo] = useState(currentNetwork)
 
     useEffect(() => {
         const unsubscribe = NetInfo.addEventListener(state => {
-            console.log("Connection type", state.type);
-            console.log("Is connected?", state.isConnected);
+            // console.log("Connection type", state.type);
+            // console.log("Is connected?", state.isConnected);
             setNetInfo(state.isConnected);
         });
         return () => unsubscribe();
